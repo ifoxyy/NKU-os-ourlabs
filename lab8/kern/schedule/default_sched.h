ad40955db98506b32beff19a95952f0cbf9b8bcc
ifoxyy <foxcier@outlook.com>
2025-12-31 15:51:49 +0800

    Add files via upload

---
+++ lab8/kern/schedule/default_sched.h
@@ -0,0 +1,12 @@
+#ifndef __KERN_SCHEDULE_SCHED_RR_H__
+#define __KERN_SCHEDULE_SCHED_RR_H__
+
+#include <sched.h>
+
+extern struct sched_class default_sched_class;
+
+/* stride scheduler migration helper (implemented in default_sched_stride.c) */
+void stride_switch_in(struct run_queue *rq);
+
+#endif /* !__KERN_SCHEDULE_SCHED_RR_H__ */
+
